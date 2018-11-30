1681d28464e9b4e48f5c7c1881e8a01282ad9b63
Anatolii_Larionov <Anatolii_Larionov@epam.com>
2018-11-30 16:17:13 +0400

    complete task 4

--- src/main/java/regexp/RegexFormatting.java
+++ src/main/java/regexp/RegexFormatting.java
@@ -1,54 +1,41 @@
 package regexp;
 
-/* У вас есть файл с текстом, смешанным с телефонными номерами, в следующем формате:
-+ Y (XXX) XXX XX XX Пример строки текста: «Уведомление было отправлено 150 раз до +4 (351) 455 22 44 успешно.
-+1 (431 ) 542 56 12 недоступен. «Использование regexp и matchers обрабатывает такой файл и создает новый
- с номерами телефонов в формате YXXXXXXXXXX.*/
-
 import java.io.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class RegexFormatting {
 
-    public void formattingPhoneNumber(String inFile, String outFile) throws IOException {
-        String str;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inFile));
-             FileOutputStream fileOutputStream = new FileOutputStream(outFile)) {
-            while ((str = bufferedReader.readLine()) != null) {
-                Pattern pattern = Pattern.compile("(\\+\\d)(\\(\\d{3}\\))([ ]\\d{3}[ ]\\d{2}[ ]\\d{2})");
-                Matcher matcher = pattern.matcher(str);
-                while (matcher.find()) {
-                    String correctPhoneNumber = matcher.group()
-                            .replaceAll("\\W", "");
-                    fileOutputStream.write(correctPhoneNumber.getBytes());
-                    fileOutputStream.write('\n');
-                    System.out.println(correctPhoneNumber);
-                }
-            }
-        }
-    }
+    private static final String IN_FILE = "src/main/resources/fails/inFile.txt";
+    private static final String OUT_FILE = "src/main/resources/fails/outFile.txt";
 
-/*    public void adasd() {
+    private String formattingPhoneNumber(String textFile) {
         Pattern pattern = Pattern.compile("(\\+\\d)(\\(\\d{3}\\))([ ]\\d{3}[ ]\\d{2}[ ]\\d{2})");
-        Matcher matcher = pattern.matcher();
+        Matcher matcher = pattern.matcher(textFile);
+        String correctNumberPhone = "";
         while (matcher.find()) {
-            String correctPhoneNumber = matcher.group()
-                    .replaceAll("\\W", "");
-            System.out.println(correctPhoneNumber);
+            String phone = matcher.group().replaceAll("\\W","");
+            correctNumberPhone = correctNumberPhone.concat(phone + "\n");
         }
+        return correctNumberPhone;
     }
 
-    public void failIn(String inFile, String outFile) throws IOException {
+    private String readFromFile() throws IOException {
+        String s = "";
+        try (BufferedReader reader = new BufferedReader(new FileReader(IN_FILE))) {
+            s = reader.readLine();
+        } catch (FileNotFoundException e) {
+            e.getMessage();
+        }
+        return s;
+    }
 
-        String str;
-        try (BufferedReader bufferedReader = new BufferedReader(new FileReader(inFile));
-             FileOutputStream fileOutputStream = new FileOutputStream(outFile)) {
-                 while ((str = bufferedReader.readLine()) != null) {
-                     adasd();
-                     fileOutputStream.write(str.getBytes());
-                     fileOutputStream.write('\n');
-            }
-         */
+    public void createAndWrite() {
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(OUT_FILE))) {
+            String resultText = formattingPhoneNumber(readFromFile());
+            writer.write(resultText);
+        }catch (IOException e) {
+            e.getMessage();
+        }
+    }
 }
-
--- src/main/java/regexp/RunnerMain.java
+++ src/main/java/regexp/RunnerMain.java
@@ -1,15 +1,9 @@
 package regexp;
 
-import java.io.IOException;
-
 public class RunnerMain {
 
-    private static final String IN_FILE = "src/main/resources/fails/inFile.txt";
-    private static final String OUT_FILE = "src/main/resources/fails/outFile.txt";
-
-    public static void main(String[] args) throws IOException {
-
+    public static void main(String[] args) {
         RegexFormatting regexFormatting = new RegexFormatting();
-        regexFormatting.formattingPhoneNumber(IN_FILE, OUT_FILE);
+        regexFormatting.createAndWrite();
     }
 }
